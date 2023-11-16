9b5a58d6aab86b0f5f015a5778eb49f7ca3988eb
ali <alioliaee31@gmail.com>
2023-11-16 21:34:57 +0330

    add result screen bottom sheet

--- src/screens/ResultScreen/index.tsx
+++ src/screens/ResultScreen/index.tsx
@@ -1,44 +1,69 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
-import UploadImageModal from '../../components/UploadImageModal/UploadImageModal';
-import {Image} from 'react-native-image-crop-picker';
-import {uploadImage} from '../../repositories/MainRepository';
+import React, {useCallback, useMemo, useRef} from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import MainLayout from '../../layouts/MainLayout';
+import BottomSheet from '@gorhom/bottom-sheet';
+import styles from './styles';
 
-const ResultScreen: React.FC = () => {
-  const [visible, setVisible] = useState(false);
-  const [image, setImage] = useState<Partial<Image> | null>(null);
+const PreviewScreen: React.FC = ({navigation}: any) => {
+  const bottomSheetRef = useRef<any>(null);
 
-  useEffect(() => {
-    if (image) {
-      uploadImage(image?.data as string)
-        .then(res => console.log(res))
-        .catch(e => console.log(e));
-    }
-  }, [image]);
+  const snapPoints = useMemo(() => ['8%', '80%'], []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Upload image</Text>
-      <PrimaryButton
-        title="File"
-        onPress={() => setVisible(true)}
-        styles={{
-          width: '90%',
-        }}
-      />
-      <UploadImageModal
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        onChange={setImage}
-      />
-    </View>
+    <MainLayout>
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.container}>
+            <Image
+              source={require('../../assets/images/preview.png')}
+              style={styles.image}
+            />
+          </View>
+          <BottomSheet
+            ref={bottomSheetRef}
+            backgroundStyle={{
+              backgroundColor: '#2D2D2D',
+              borderColor: '#fff',
+              borderWidth: 1,
+            }}
+            index={1}
+            snapPoints={snapPoints}
+            handleIndicatorStyle={{display: 'none'}}
+            onChange={handleSheetChanges}>
+            <View style={styles.modalContentContainer}>
+              <View style={styles.modalHeader}>
+                <Text style={styles.languageText}>English</Text>
+                <View style={styles.modalOptions}>
+                  <TouchableOpacity style={styles.option}>
+                    <Image
+                      source={require('../../assets/images/speaker.png')}
+                      style={styles.icon}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.option}>
+                    <Image
+                      source={require('../../assets/images/copy.png')}
+                      style={styles.icon}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.option}>
+                    <Image
+                      source={require('../../assets/images/close.png')}
+                      style={styles.icon}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </BottomSheet>
+        </View>
+      </>
+    </MainLayout>
   );
 };
 
-export default ResultScreen;
+export default PreviewScreen;
--- src/screens/ResultScreen/styles.tsx
+++ src/screens/ResultScreen/styles.tsx
@@ -1,5 +1,74 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    height: '90%',
+    backgroundColor: '#2D2D2D',
+    alignSelf: 'center',
+    borderRadius: 20,
+    marginTop: 50,
+  },
+  header: {
+    flex: 1,
+    paddingTop: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  sheet: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  button: {
+    backgroundColor: 'red',
+    padding: 8,
+    borderRadius: 10,
+    minWidth: 100,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  modalContainer: {
+    flex: 1,
+  },
+  modalContentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#2D2D2D',
+  },
+  modalHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  modalOptions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  option: {
+    marginEnd: 14,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    resizeMode: 'contain',
+  },
+  languageText: {
+    color: '#fff',
+    marginStart: 20,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
 
 export default styles;
