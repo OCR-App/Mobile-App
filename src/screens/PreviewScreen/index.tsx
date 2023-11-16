056e7ad092ea411b6af4e8a3694b66bc8fe95737
ali <alioliaee31@gmail.com>
2023-11-16 19:55:05 +0330

    create preview screen

--- src/screens/PreviewScreen/index.tsx
+++ src/screens/PreviewScreen/index.tsx
@@ -1,43 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
-import UploadImageModal from '../../components/UploadImageModal/UploadImageModal';
-import {Image} from 'react-native-image-crop-picker';
-import {uploadImage} from '../../repositories/MainRepository';
+import React from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import MainLayout from '../../layouts/MainLayout';
+import styles from './styles';
 
-const PreviewScreen: React.FC = () => {
-  const [visible, setVisible] = useState(false);
-  const [image, setImage] = useState<Partial<Image> | null>(null);
-
-  useEffect(() => {
-    if (image) {
-      uploadImage(image?.data as string)
-        .then(res => console.log(res))
-        .catch(e => console.log(e));
-    }
-  }, [image]);
-
+const PreviewScreen: React.FC = ({navigation}: any) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Upload image</Text>
-      <PrimaryButton
-        title="File"
-        onPress={() => setVisible(true)}
-        styles={{
-          width: '90%',
-        }}
-      />
-      <UploadImageModal
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        onChange={setImage}
-      />
-    </View>
+    <MainLayout>
+      <>
+        <View style={styles.header}>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            style={[
+              styles.button,
+              {
+                backgroundColor: '#2D2D2D',
+                borderWidth: 1,
+                borderColor: '#fff',
+              },
+            ]}>
+            <Text style={styles.buttonText}>Try Again</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Result')}
+            style={[
+              styles.button,
+              {
+                backgroundColor: '#2E8B57',
+              },
+            ]}>
+            <Text style={styles.buttonText}>Confirm</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <Image
+            source={require('../../assets/images/preview.png')}
+            style={styles.image}
+          />
+        </View>
+      </>
+    </MainLayout>
   );
 };
 
--- src/screens/PreviewScreen/styles.tsx
+++ src/screens/PreviewScreen/styles.tsx
@@ -1,5 +1,37 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    height: '80%',
+    backgroundColor: '#2D2D2D',
+    alignSelf: 'center',
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+  header: {
+    flex: 1,
+    paddingTop: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  button: {
+    backgroundColor: 'red',
+    padding: 8,
+    borderRadius: 10,
+    minWidth: 100,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
 
 export default styles;
