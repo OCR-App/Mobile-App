d605507eafcb6854361b14ab05cd2d965c4501b0
ali <alioliaee31@gmail.com>
2023-11-17 13:07:43 +0330

    align loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 import Modal from 'react-native-modal';
 import {useAppSelector} from '../../store/hooks';
+import styles from './styles';
 
 const Loading: React.FC = () => {
   const {isLoading} = useAppSelector(state => state.core);
@@ -12,14 +13,12 @@ const Loading: React.FC = () => {
       statusBarTranslucent
       onDismiss={() => {}}
       onBackButtonPress={() => {}}
+      style={styles.modal}
       onBackdropPress={() => {}}>
       <LottieView
         source={require('../../assets/loading.json')}
         autoPlay
-        style={{
-          width: 300,
-          height: 300,
-        }}
+        style={styles.lottie}
       />
     </Modal>
   );
--- src/components/Loading/styles.tsx
+++ src/components/Loading/styles.tsx
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  modal: {
     flex: 1,
     position: 'absolute',
     zIndex: 99,
@@ -11,6 +11,14 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
+    margin: 0,
+    padding: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottie: {
+    width: 300,
+    height: 300,
   },
 });
 
